3f1202eade8b45a5efa8353cc0ba6ac293af4d95
johnkueh <john.kueh@gmail.com>
2021-01-30 15:46:14 +1100

    Embed youtube

--- notion-page/PageRenderer.tsx
+++ notion-page/PageRenderer.tsx
@@ -1,6 +1,6 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { AspectRatio, Box, Heading, Text } from "@chakra-ui/react";
 import React from "react";
-import { NotionRenderer } from "react-notion";
+import { ContentValueType, NotionRenderer } from "react-notion";
 import Link from "../shared/Link";
 
 const PageRenderer = ({ page, blockMap }) => {
@@ -19,23 +19,23 @@ const PageRenderer = ({ page, blockMap }) => {
           },
         }}
         customBlockComponents={{
-          video: ({
-            blockValue,
-            blockValue: {
-              properties: { caption, source },
-            },
-          }) => {
-            return (
-              <Box>
-                <video width="400" controls>
-                  <source src={source} type="video/mov" />
-                </video>
-                <div>
-                  {caption} / {source}
-                </div>
-                <div>{JSON.stringify(blockValue)}</div>
-              </Box>
-            );
+          video: ({ blockValue }) => {
+            const { format } = blockValue as ContentValueType;
+            if (format) {
+              return (
+                <Box>
+                  <AspectRatio
+                    maxW={format.block_width}
+                    ratio={1 / format.block_aspect_ratio}
+                  >
+                    <iframe
+                      src={`${format.display_source}&modestbranding=1&showinfo=0&rel=0&cc_load_policy=1&iv_load_policy=3&theme=light&fs=0`}
+                      frameBorder="0"
+                    />
+                  </AspectRatio>
+                </Box>
+              );
+            }
           },
         }}
       />
