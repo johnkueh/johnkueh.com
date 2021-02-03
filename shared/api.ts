2d1cfe7fca0919132a6fab8739d845848f72138d
johnkueh <john.kueh@gmail.com>
2021-02-04 10:05:37 +1100

    Only show published posts

--- shared/api.ts
+++ shared/api.ts
@@ -1,7 +1,13 @@
+function filterPublished(collection: any) {
+  return collection.filter(entry => entry['Status'] === "Published")
+}
+
 export async function getProjects() {
-  return fetch(
+  const data = await fetch(
     "https://notion-api.splitbee.io/v1/table/2bce795a169044aeb0aa5b6b5a30b0a7"
   ).then((res) => res.json());
+
+  return filterPublished(data);
 }
 
 export async function getPageData(slug: string) {
@@ -33,9 +39,11 @@ export async function getPageSlugById(id: string) {
 }
 
 export async function getArticles() {
-  return fetch(
+  const data = await fetch(
     "https://notion-api.splitbee.io/v1/table/2d9b070b663a4b4380a415581d9aa00e"
   ).then((res) => res.json());
+
+  return filterPublished(data);
 }
 
 export async function getArticleData(slug: string) {
@@ -55,7 +63,9 @@ export async function getArticle(slug: string) {
 }
 
 export async function getTestimonials() {
-  return fetch(
+  const data = await fetch(
     "https://notion-api.splitbee.io/v1/table/719bbddfe8374a60a7524f466cb05176"
   ).then((res) => res.json());
+
+  return filterPublished(data);
 }
